1f217f6270a0455979034642e186cf027fe71555
titli1981 <madhumishra084@gmail.com>
2017-02-08 12:13:30 -0500

    Revert "Added create action to save to the database"

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,19 +1,4 @@
 class ContactsController < ApplicationController
- def new
-   @contact = Contact.new
- end
-def create
-  @contact = Contact.new(contact_params)
-  if @contact.save
-    redirect_to new_contact_path, notice: "Message sent."
-  else
-    redirect_to new_contact_path, notice: "Error occured."
+  def new
   end
-end
- 
-private
-  def contact_params
-    params.require(:contact).permit(:name, :email, :comments)
-  end
-  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 20170127172853) do
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.text     "comments"
+    t.text     "contacts"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
